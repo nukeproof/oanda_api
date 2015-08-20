9f2fea5229546e094b704386476666f80b77b61c
Eric Thompson <ethompson@momentumsi.com>
2015-08-19 21:24:52 -0500

    Increased pool size to 10

--- lib/oanda_api/client/client.rb
+++ lib/oanda_api/client/client.rb
@@ -16,7 +16,7 @@ module OandaAPI
     include HTTParty
     persistent_connection_adapter idle_timeout: 10,
                                   keep_alive: 30,
-                                  pool_size: 2
+                                  pool_size: 10
 
     # Use a custom JSON parser
     parser OandaAPI::Client::JsonParser
